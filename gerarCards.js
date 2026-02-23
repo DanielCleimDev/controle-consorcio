414a9e330281e17d7936d2ff4f1b635ecb87de3d
DanielCleim <danielsgsmg@gmail.com>
2026-02-23 16:15:01 -0300

    teste 2 geraCards() no celular

--- gerarCards.js
+++ gerarCards.js
@@ -5,15 +5,19 @@ export function gerarCards(){
     alert("iniciou");
     app = document.getElementById("app");
     app.textContent = "";
-    for (let index = 1; index <= 5000; index++) {
+    for (let index = 1; index <= 5; index++) {
         let div = document.createElement("div");
         div.id = `card${index}`;
-
+        alert(index);
         if(verificaChave(index)){
+            alert("entrou no Verifica chave")
             div.className = `elemento ${getLocalStorage(index)}`;
         }else{
+            alert("entrou no else do verifica chave")
             div.className = `elemento`;
         }
+
+        alert(String(index).padStart(4,'0'))
 
         div.textContent = String(index).padStart(4,'0');
         app.appendChild(div)
